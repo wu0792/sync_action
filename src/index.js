78de4f014905d4809b052a99f5c02ea7e7ba5d48
wumm <wumm@Ctrip.com>
2018-11-20 20:45:37 +0800

    remove reason ml, pure react js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Popup } from './js/popup';
+
+ReactDOM.render(<Popup />, document.getElementById('root'))
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,46 @@
-/* This output is empty. Its source's type definitions, externals and/or unused code got optimized away. */
+/*global chrome:true*/
+
+import React, { useState, useEffect } from 'react'
+
+function sendMessage(data) {
+  debugger
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0]
+    chrome.tabs.sendMessage(activeTab.id, data)
+  })
+}
+
+const Popup = () => {
+  let [active, setActive] = useState(false)
+  let [asMainWindow, setAsMainWindow] = useState(false)
+
+  useEffect(() => {
+    sendMessage({ action: 'changeActive', value: active })
+  }, [active])
+
+  useEffect(() => {
+    sendMessage({ action: 'changeAsMainWindow', value: asMainWindow })
+  }, [asMainWindow])
+
+  return (
+    <React.Fragment>
+      <div>
+        <label htmlFor="active">是否激活：</label>
+        <label>
+          <input type="checkbox" name="active" id="active" checked={active} onChange={_ => setActive(!active)} />
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="isMainWindow">是否主窗口：</label>
+        <label>
+          <input type="checkbox" name="isMainWindow" id="isMainWindow" checked={asMainWindow} onChange={_ => setAsMainWindow(!asMainWindow)} />
+        </label>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export {
+  Popup
+}
